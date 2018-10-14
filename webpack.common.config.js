d6e2e27725b9f0780d121243fd21807ef83623c2
Haisheng Wu <haisheng.wu@okta.com>
2018-10-14 00:49:06 -0700

    fixing webpack build

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -6,6 +6,11 @@ require('./writeConfig');
 module.exports = {
   module: {
     loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
       { test: /\.json$/, loader: 'json' }
     ]
   }
